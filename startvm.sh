ef499aa788c7202f0fb3c6fce533cf7b9579dc7a
SeongJae Park <sj38.park@gmail.com>
2018-08-08 14:42:45 +0900

    startvm: Allow number of CPUs and ram size to be configurable
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- startvm.sh
+++ startvm.sh
@@ -3,13 +3,27 @@
 BINDIR=`dirname $0`
 cd $BINDIR
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-	echo "Usage: $0 <path to disk file>"
+	echo "Usage: $0 <path to disk file> [<nr_cores> [ram size]]"
 	exit 1
 fi
 
 DISK=$1
 
-./bin/x86_64-softmmu/qemu-system-x86_64 -m 64G -smp 16 -enable-kvm \
+NR_CORES=16
+if [ $# -gt 1 ]
+then
+	NR_CORES=$2
+fi
+
+SZ_RAM=32G
+if [ $# -gt 2 ]
+then
+	SZ_RAM=$3
+fi
+
+echo cores: $NR_CORES, ram: $SZ_RAM
+
+./bin/x86_64-softmmu/qemu-system-x86_64 -m $SZ_RAM -smp $NR_CORES -enable-kvm \
 	-drive if=virtio,file=$DISK,cache=none -redir tcp:2242::22 -nographic
