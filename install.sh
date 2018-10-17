edce01fa90344dc734e58cf56b34e13e330f8911
SeongJae Park <sj38.park@gmail.com>
2018-10-17 14:29:54 +0900

    install.sh: Install pre-requisites
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 BINDIR=`dirname $0`
 cd $BINDIR
 
+sudo apt install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev \
+	libgtk2.0-dev
+
 if [ ! -d qemu ]
 then
 	git clone git://git.qemu-project.org/qemu.git
