cf3fbf4f8506c79c0bb108deb9effd24291b1089
SeongJae Park <sj38.park@gmail.com>
2021-02-11 07:37:19 +0100

    install.sh: Fix some shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bindir=$(dirname $0)
-cd $bindir
+bindir=$(dirname "$0")
+cd "$bindir" || exit 1
 
 sudo apt install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev \
 	libgtk-3-dev libncurses5-dev libncursesw5-dev ninja-build
@@ -10,12 +10,12 @@ if [ ! -d qemu ]
 then
 	git clone git://git.qemu-project.org/qemu.git
 fi
-cd qemu
+cd qemu || exit 1
 git remote update
 git checkout v5.2.0
 
 mkdir ../bin
-cd ../bin
+cd ../bin || exit 1
 ../qemu/configure --enable-debug --enable-gtk --enable-curses
 NR_CPUS=$(grep "^processor" /proc/cpuinfo | wc -l)
-make -j$NR_CPUS
+make -j"$NR_CPUS"
